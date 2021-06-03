d3ec2c5f9de73a55a839f5fa6ef47db2939c90ec
haem0212 <haem.02121999@gmail.com>
2021-06-03 17:30:55 +0700

    kai update

---
+++ screens/DrawerContent.js
@@ -0,0 +1,189 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import {
+    useTheme,
+    Avatar,
+    Title,
+    Caption,
+    Paragraph,
+    Drawer,
+    Text,
+    TouchableRipple,
+    Switch
+} from 'react-native-paper';
+import {
+    DrawerContentScrollView,
+    DrawerItem
+} from '@react-navigation/drawer';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import{ AuthContext } from '../components/context';
+
+export function DrawerContent(props) {
+
+    const paperTheme = useTheme();
+
+    const { signOut, toggleTheme } = React.useContext(AuthContext);
+
+    return(
+        <View style={{flex:1}}>
+            <DrawerContentScrollView {...props}>
+                <View style={styles.drawerContent}>
+                    <View style={styles.userInfoSection}>
+                        <View style={{flexDirection:'row',marginTop: 15}}>
+                            <Avatar.Image 
+                                source={{
+                                    uri: 'https://scontent.fhan2-2.fna.fbcdn.net/v/t1.6435-9/147233031_1356456018051948_6473320015563748717_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=3B_eO3oKt2IAX8avain&_nc_ht=scontent.fhan2-2.fna&oh=1b839d8c7a2e0fcd621dacea700ddae3&oe=60DD9BA2'
+                                }}
+                                size={50}
+                            />
+                            <View style={{marginLeft:15, flexDirection:'column'}}>
+                                <Title style={styles.title}>Ha Em </Title>
+                                <Caption style={styles.caption}>@Kai1212</Caption>
+                            </View>
+                        </View>
+
+                        <View style={styles.row}>
+                            <View style={styles.section}>
+                                <Paragraph style={[styles.paragraph, styles.caption]}>80</Paragraph>
+                                <Caption style={styles.caption}>Theo dõi</Caption>
+                            </View>
+                            <View style={styles.section}>
+                                <Paragraph style={[styles.paragraph, styles.caption]}>100</Paragraph>
+                                <Caption style={styles.caption}>Lượt theo dõi</Caption>
+                            </View>
+                        </View>
+                    </View>
+
+                    <Drawer.Section style={styles.drawerSection}>
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="home-outline" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Trang chủ"
+                            onPress={() => {props.navigation.navigate('Home')}}
+                        />
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="account-outline" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Trang cá nhân"
+                            onPress={() => {props.navigation.navigate('Profile')}}
+                        />
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="bookmark-outline" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Đánh dấu"
+                            onPress={() => {props.navigation.navigate('BookmarkScreen')}}
+                        />
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="settings-outline" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Cài đặt"
+                            onPress={() => {props.navigation.navigate('SettingScreen')}}
+                        />
+                        <DrawerItem 
+                            icon={({color, size}) => (
+                                <Icon 
+                                name="account-check-outline" 
+                                color={color}
+                                size={size}
+                                />
+                            )}
+                            label="Hỗ trợ"
+                            onPress={() => {props.navigation.navigate('SupportScreen')}}
+                        />
+                    </Drawer.Section>
+                    <Drawer.Section title="Hiển thị">
+                        <TouchableRipple onPress={() => {toggleTheme()}}>
+                            <View style={styles.preference}>
+                                <Text>Màu tối</Text>
+                                <View pointerEvents="none">
+                                    <Switch value={paperTheme.dark}/>
+                                </View>
+                            </View>
+                        </TouchableRipple>
+                    </Drawer.Section>
+                </View>
+            </DrawerContentScrollView>
+            <Drawer.Section style={styles.bottomDrawerSection}>
+                <DrawerItem 
+                    icon={({color, size}) => (
+                        <Icon 
+                        name="exit-to-app" 
+                        color={color}
+                        size={size}
+                        />
+                    )}
+                    label="Đăng xuất"
+                    onPress={() => {signOut()}}
+                />
+            </Drawer.Section>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    drawerContent: {
+      flex: 1,
+    },
+    userInfoSection: {
+      paddingLeft: 20,
+    },
+    title: {
+      fontSize: 16,
+      marginTop: 3,
+      fontWeight: 'bold',
+    },
+    caption: {
+      fontSize: 14,
+      lineHeight: 14,
+    },
+    row: {
+      marginTop: 20,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    section: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginRight: 15,
+    },
+    paragraph: {
+      fontWeight: 'bold',
+      marginRight: 3,
+    },
+    drawerSection: {
+      marginTop: 15,
+    },
+    bottomDrawerSection: {
+        marginBottom: 15,
+        borderTopColor: '#f4f4f4',
+        borderTopWidth: 1
+    },
+    preference: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      paddingVertical: 12,
+      paddingHorizontal: 16,
+    },
+  });
---
+++ screens/MainTabScreen.js
@@ -0,0 +1,238 @@
+import React from 'react';
+
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Icon from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import HomeScreen from './HomeScreen';
+import NotificationScreen from './NotificationScreen';
+import ExploreScreen from './ExploreScreen';
+import ProfileScreen from './ProfileScreen';
+import MapTestScreen from './MapTestScreen';
+import EditProfileScreen from './EditProfileScreen';
+
+import {useTheme, Avatar} from 'react-native-paper';
+import {View} from 'react-native-animatable';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import CardListScreen from './CardListScreen';
+import CardItemDetails from './CardItemDetails';
+
+const HomeStack = createStackNavigator();
+const NotificationStack = createStackNavigator();
+const ProfileStack = createStackNavigator();
+
+const Tab = createMaterialBottomTabNavigator();
+
+const MainTabScreen = () => (
+  <Tab.Navigator initialRouteName="Trang chủ" activeColor="#fff">
+    <Tab.Screen
+      name="Home"
+      component={HomeStackScreen}
+      options={{
+        tabBarLabel: 'Trang chủ',
+        tabBarColor: '#FF6347',
+        tabBarIcon: ({color}) => (
+          <Icon name="ios-home" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Updates"
+      component={NotificationStackScreen}
+      options={{
+        tabBarLabel: 'Thông báo',
+        tabBarColor: '#1f65ff',
+        tabBarIcon: ({color}) => (
+          <Icon name="ios-notifications" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Profile"
+      component={ProfileStackScreen}
+      options={{
+        tabBarLabel: 'Trang cá nhân',
+        tabBarColor: '#694fad',
+        tabBarIcon: ({color}) => (
+          <Icon name="ios-person" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Khám phá"
+      component={ExploreScreen}
+      options={{
+        tabBarLabel: 'Khám phá',
+        tabBarColor: '#d02860',
+        tabBarIcon: ({color}) => (
+          <Icon name="ios-aperture" color={color} size={26} />
+        ),
+      }}
+    />
+  </Tab.Navigator>
+);
+
+export default MainTabScreen;
+
+const HomeStackScreen = ({navigation}) => {
+  const {colors} = useTheme();
+  return (
+    <HomeStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.background,
+          shadowColor: colors.background, // iOS
+          elevation: 0, // Android
+        },
+        headerTintColor: colors.text,
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: '3TFood',
+          headerLeft: () => (
+            <View style={{marginLeft: 10}}>
+              <Icon.Button
+                name="ios-menu"
+                size={25}
+                color={colors.text}
+                backgroundColor={colors.background}
+                onPress={() => navigation.openDrawer()}
+              />
+            </View>
+          ),
+          headerRight: () => (
+            <View style={{flexDirection: 'row', marginRight: 10}}>
+              <Icon.Button
+                name="ios-search"
+                size={25}
+                color={colors.text}
+                backgroundColor={colors.background}
+                onPress={() => {}}
+              />
+              <TouchableOpacity
+                style={{paddingHorizontal: 10, marginTop: 5}}
+                onPress={() => {
+                  navigation.navigate('Profile');
+                }}>
+                <Avatar.Image
+                  source={{
+                    uri:
+                      'https://scontent.fhan2-2.fna.fbcdn.net/v/t1.6435-9/147233031_1356456018051948_6473320015563748717_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=3B_eO3oKt2IAX8avain&_nc_ht=scontent.fhan2-2.fna&oh=1b839d8c7a2e0fcd621dacea700ddae3&oe=60DD9BA2',
+                  }}
+                  size={30}
+                />
+              </TouchableOpacity>
+            </View>
+          ),
+        }}
+      />
+      <HomeStack.Screen 
+        name="CardListScreen"
+        component={CardListScreen}
+        options={({route}) => ({
+          title: route.params.title,
+          headerBackTitleVisible: false
+        })}
+      />
+      <HomeStack.Screen 
+        name="CardItemDetails"
+        component={CardItemDetails}
+        options={({route}) => ({
+          // title: route.params.title,
+          headerBackTitleVisible: false,
+          headerTitle: false,
+          headerTransparent: true,
+          headerTintColor: '#fff'
+        })}
+      />
+    </HomeStack.Navigator>
+  );
+};
+
+const NotificationStackScreen = ({navigation}) => (
+  <NotificationStack.Navigator
+    screenOptions={{
+      headerStyle: {
+        backgroundColor: '#1f65ff',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    }}>
+    <NotificationStack.Screen
+      name="Notifications"
+      component={NotificationScreen}
+      options={{
+        headerLeft: () => (
+          <Icon.Button
+            name="ios-menu"
+            size={25}
+            backgroundColor="#1f65ff"
+            onPress={() => navigation.openDrawer()}
+          />
+        ),
+      }}
+    />
+  </NotificationStack.Navigator>
+);
+
+const ProfileStackScreen = ({navigation}) => {
+  const {colors} = useTheme();
+
+  return (
+    <ProfileStack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: colors.background,
+          shadowColor: colors.background, // iOS
+          elevation: 0, // Android
+        },
+        headerTintColor: colors.text,
+      }}>
+      <ProfileStack.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: '',
+          headerLeft: () => (
+            <View style={{marginLeft: 10}}>
+              <Icon.Button
+                name="ios-menu"
+                size={25}
+                backgroundColor={colors.background}
+                color={colors.text}
+                onPress={() => navigation.openDrawer()}
+              />
+            </View>
+          ),
+          headerRight: () => (
+            <View style={{marginRight: 10}}>
+              <MaterialCommunityIcons.Button
+                name="account-edit"
+                size={25}
+                backgroundColor={colors.background}
+                color={colors.text}
+                onPress={() => navigation.navigate('EditProfile')}
+              />
+            </View>
+          ),
+        }}
+      />
+      <ProfileStack.Screen
+        name="EditProfile"
+        options={{
+          title: 'Edit Profile',
+        }}
+        component={EditProfileScreen}
+      />
+    </ProfileStack.Navigator>
+  );
+};
